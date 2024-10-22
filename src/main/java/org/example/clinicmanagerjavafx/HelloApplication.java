1c005fcc56f44372b8d60529d50b4b749e1c9152
Amit Deshpande <amitdeshpande@theinke.com>
2024-10-22 10:19:38 -0400

    FXML linked complete, application runs

--- src/main/java/org/example/clinicmanagerjavafx/HelloApplication.java
+++ src/main/java/org/example/clinicmanagerjavafx/HelloApplication.java
@@ -1,63 +1,21 @@
 package org.example.clinicmanagerjavafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
-
-import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
-public class HelloApplication<ActionEvent> extends Application {
-    @FXML
-    private DatePicker appointmentDatePicker;
-
-    @FXML
-    private Pane appointmentInfoPane;
-
-    @FXML
-    private ChoiceBox<?> chosenProvider;
-
-    @FXML
-    private ChoiceBox<?> chosenTimeslot;
-
-    @FXML
-    private Pane officeAppointmentSelectorPane;
-
-    @FXML
-    private Pane imagingAppointmentSelectorPane;
-
-    @FXML
-    private Pane visitTypePane;
-
-    @FXML
-    private RadioButton officeRadioButton;
-
-    @FXML
-    private RadioButton imagingRadioButton;
-
-    @FXML
-    void showOfficeDetailSelector(ActionEvent event) {
-        // office radio button is selected, which triggers this function
-        // show officeAppointmentSelectorPane
-    }
-
-    @FXML
-    void showImagingDetailSelector(ActionEvent event){}
-    // imaging radio button is selected, which triggers this function
-    // show imagingAppointmentSelectorPane
-
+public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
         stage.setTitle("Clinic Manager");
-        stage.setScene(scene);
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
         stage.show();
     }
 
--- src/main/java/org/example/clinicmanagerjavafx/HelloController.java
+++ src/main/java/org/example/clinicmanagerjavafx/HelloController.java
@@ -1,14 +1,55 @@
 package org.example.clinicmanagerjavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.Pane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private DatePicker appointmentDatePicker;
+
+    @FXML
+    private Pane appointmentInfoPane;
+
+    @FXML
+    private ChoiceBox<?> chosenProvider;
+
+    @FXML
+    private ChoiceBox<?> chosenTimeslot;
+
+    @FXML
+    private Pane officeAppointmentSelectorPane;
+
+    @FXML
+    private Pane imagingAppointmentSelectorPane;
+
+    @FXML
+    private Pane visitTypePane;
+
+    @FXML
+    private RadioButton officeRadioButton;
+
+    @FXML
+    private RadioButton imagingRadioButton;
+
+    @FXML
+    void showOfficeDetailSelector(javafx.event.ActionEvent event) {
+        // office radio button is selected, which triggers this function
+        // show officeAppointmentSelectorPane
+        System.out.println("Office radio button clicked");
+        officeAppointmentSelectorPane.setVisible(true);
+        imagingAppointmentSelectorPane.setVisible(false);
+    }
+
+    @FXML
+    void showImagingDetailSelector(javafx.event.ActionEvent event) {
+        // imaging radio button is selected, which triggers this function
+        // show imagingAppointmentSelectorPane
+        System.out.println("Imaging radio button clicked");
+        imagingAppointmentSelectorPane.setVisible(true);
+        officeAppointmentSelectorPane.setVisible(false);
     }
 }
